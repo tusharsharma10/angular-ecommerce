cedd2c09290a885725c5d7f2581be48355c033ea
lenovo <sharmatushar759@gmail.com>
2020-04-30 01:32:15 +0530

    displaying product by categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
+import { CategoryService } from '../service/category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent  {
+export class ProductsComponent {
 
-products$;
+  products: any[];
+  filteredProducts: any[];
+  categories$;
+  category: string;
 
-  constructor(private productService:ProductService) { 
+  constructor(private productService: ProductService,
+    categoryService: CategoryService, route: ActivatedRoute) {
 
-    this.products$ = productService.getAll().snapshotChanges();
+    productService
+      .getAll()
+      .snapshotChanges()
+      .subscribe(products => this.filteredProducts = this.products = products);
+
+    this.categories$ = categoryService.getCategories().snapshotChanges();
+
+    route.queryParamMap.subscribe(params => {
+      
+      this.category = params.get('category');
+
+  this.filteredProducts = (this.category) ? 
+                          this.products.filter( p => p.payload.val().category === this.category) :
+                          this.products;
+
+    });
+
+
+
+
   }
 
-  
+
 
 }
